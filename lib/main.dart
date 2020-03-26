ad096f52d2917bd16a1e6beae6561e9f19d60904
Hwan-seok <tttkhs@naver.com>
2020-03-27 02:55:36 +0900

    (app) created my profile app

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
-                child: Text("Hello"),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/hwan.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 100,
-                    width: 100,
+              Text(
+                'Hwan Seok Kang',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'CTO',
+                style: TextStyle(
+                    fontFamily: 'SansPro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10,
+                width: 200,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                  thickness: 1,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        size: 50,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        "+82 010 6430 6352",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal,
+                            fontFamily: 'SansPro',
+                            fontSize: 24),
+                      )),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(50, 0, 50, 30),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 50,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "tttkhs@naver.com",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal,
+                          fontFamily: 'SansPro',
+                          fontSize: 24),
+                    ),
                   ),
-                  Container(
-                    color: Colors.green,
-                    height: 100,
-                    width: 100,
-                  )
-                ],
-              ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-                child: Text("Hello"),
-              ),
+                ),
+              )
             ],
           ),
         ),
