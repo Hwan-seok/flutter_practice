f2003c82dfc577c328158f24fda5048ae600c4b3
Hwan-seok <tttkhs@naver.com>
2020-03-28 03:47:18 +0900

    (practice) only stateless widget app

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/hwan.jpg'),
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print("left");
+                },
+                child: Image.asset('images/dice1.png'),
               ),
-              Text(
-                'Hwan Seok Kang',
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontFamily: 'Pacifico',
-                    fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print("right");
+                },
+                child: Image.asset('images/dice1.png'),
               ),
-              Text(
-                'CTO',
-                style: TextStyle(
-                    fontFamily: 'SansPro',
-                    fontSize: 20,
-                    color: Colors.teal.shade100,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0),
-              ),
-              SizedBox(
-                height: 10,
-                width: 200,
-                child: Divider(
-                  color: Colors.teal.shade50,
-                  thickness: 1,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        size: 50,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        "+82 010 6430 6352",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.teal,
-                            fontFamily: 'SansPro',
-                            fontSize: 24),
-                      )),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.fromLTRB(50, 0, 50, 30),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      size: 50,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      "tttkhs@naver.com",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.teal,
-                          fontFamily: 'SansPro',
-                          fontSize: 24),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
