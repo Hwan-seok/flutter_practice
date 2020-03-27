86f26da955aef43727eef1882f63aff5d5214c68
Hwan-seok <tttkhs@naver.com>
2020-03-28 06:16:33 +0900

    (practice)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:math';
-
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,48 +10,16 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Center(
+          child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: Text("click"),
+          ),
+        ),
       ),
     ),
   );
-}
-
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-  void onPressDice() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                onPressed: onPressDice,
-                child: Image.asset('images/dice$leftDiceNumber.png'),
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                onPressed: onPressDice,
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
