6ace33d29e8964afe465496364cb61a561d9de22
Hwan-seok <tttkhs@naver.com>
2020-03-28 06:59:39 +0900

    (practice app) made xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(Xylo());
+
+class Xylo extends StatelessWidget {
+  void playSound(int number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  Expanded span(int sNum, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(sNum);
+        },
+        color: color,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text("click"),
-          ),
+        appBar: AppBar(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            span(1, Colors.red),
+            span(2, Colors.blueGrey),
+            span(3, Colors.indigo),
+            span(4, Colors.lightGreenAccent),
+            span(5, Colors.green),
+            span(6, Colors.teal),
+            span(7, Colors.pinkAccent),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
